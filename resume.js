533dd27036c5d8b6e05275d7556d547736cd007c
Mike Haglund <44382034+MikeHaglund@users.noreply.github.com>
2018-11-16 13:04:05 -0700

    Create resume.js

---
+++ resume.js
@@ -0,0 +1,104 @@
+<h1><p style="color:red;">Michael Haglund</h1>
+<h4>801.201.7078</h4>
+<h4>mhaglund27@gmail.com<h4>
+<h4>GitHub: MikeHaglund
+LinkedIn: Mike Haglund</h4>
+
+</div>
+
+<h2><p style="color:red;">Skills</h2>
+<p>
+
+Javascript, HTML, .CSS, Git, databases, servers, Photoshop, 
+                 
+</p>
+
+                 
+<h2><p style="color:red;">Projects</h2>
+
+<h3>1280 The Zone
+<br />
+</h3>
+<p>
+<ul>
+<li>                 
+Updated website, made more mobile friendly
+</li>
+</ul>
+</p>
+
+<h3>Salt Lake Bees</h3>
+<ul>
+<li>
+Built the official App for The Salt Lake Bees
+</li>
+</ul>
+
+ <h2><p style="color:red;">Education</h2>
+
+<h3>University of Utah
+<br/></h3>
+<p>
+<ul>
+<li>                 
+Strategic Communication Major, Sociology Minor, May 2017
+</li>
+</ul>
+</p>                         
+
+                          
+<h2><p style="color:red;">Work Experience</h2>
+<p>
+<h3>Citywide Home Loans, June 2017 - September 2018</h3>
+</p>
+
+<li>
+CRM Marketing Coordinator: supported company marketing initiatives by creating, managing and distributing 700 email marketing campaigns for 500 Loan Officers, resulting in nearly 14,000 funded loans. Assist in managing LO databases and provide technical support for the company’s Customer Relation Management system, BNTouch.
+
+<p>
+<h3>University of Utah Athletics Department, January 2017 – June 2017</h3>
+</p>
+
+<li>
+Marketing Intern: supported University athletic events by executing plans to grow attendance for baseball and softball, throwing out U of U merchandise at games, planning and executing activities between innings, and designing the new banners hanging outside Rice Eccles Stadium.
+</li>
+
+<p>
+<h3>GumCo, June 2016 – July 2016</h3>
+</p>
+
+<li>
+Marketing Intern: assisted ad agency execs in creating, designing and supporting a PSA campaign for underage marijuana use in Washington.
+</li><p>
+<h3>Cottonwood High School, February 2013 – April 2016</h3>
+</p>
+
+<li>
+Boys and Girls Water Polo Head Coach
+<li>2015 Utah State Water Polo Coach of the Year
+<li>2014 Girls team Eastern Conference Coach of the Year
+
+</li><p>
+<h3>Marriott Vacation Club, September 2014 – January 2015</h3>
+</p>
+
+<li>
+Vacation Club Planner / Customer Service: assisted clients with vacation bookings at Marriott’s timeshare properties, resolved customer service complaints, and booked Marriott hotels.
+</li>                 
+<br />
+<h2><p style="color:red;">Volunteer Experience</h2>
+<h3>Salt Lake Comic Con, September 2013</h3>
+
+<ul>
+<li>
+	Convention volunteer.
+</li>
+<li>
+	Personal assistant to The Fonz (Henry Winkler).
+</li>
+</ul>
+<h3>The Church of Jesus Christ of Latter-Day Saints, Pennsylvania Philadelphia Mission, December 2010 - December 2012</h3>
+
+<li>
+Volunteer missionary, District Leader / Trainer.
+</li>
